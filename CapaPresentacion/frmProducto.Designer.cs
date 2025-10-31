e110098205b048ea5273347579858337d8615025
Lbgraica <garciahernandezbryan@gmail.com>
2025-10-30 22:17:41 -0600

    Creacion frmProductos

---
+++ CapaPresentacion/frmProducto.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace CapaPresentacion
+{
+    partial class frmProducto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnMenu = new System.Windows.Forms.Button();
+            this.dgvGranja = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtCodigoPostalGranja = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblCodigoGranja = new System.Windows.Forms.Label();
+            this.txtCorreoGranja = new System.Windows.Forms.TextBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.txtDireccionGranja = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.Label();
+            this.txtTelefonoGranja = new System.Windows.Forms.TextBox();
+            this.txtNombreGranja = new System.Windows.Forms.TextBox();
+            this.cboxEstadoGranja = new System.Windows.Forms.ComboBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblFechaGranja = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGranja)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnSalir.Location = new System.Drawing.Point(701, 444);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(86, 35);
+            this.btnSalir.TabIndex = 60;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMenu.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnMenu.Location = new System.Drawing.Point(55, 446);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(86, 35);
+            this.btnMenu.TabIndex = 59;
+            this.btnMenu.Text = "Menu";
+            this.btnMenu.UseVisualStyleBackColor = true;
+            // 
+            // dgvGranja
+            // 
+            this.dgvGranja.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dgvGranja.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvGranja.Location = new System.Drawing.Point(9, 231);
+            this.dgvGranja.Name = "dgvGranja";
+            this.dgvGranja.ReadOnly = true;
+            this.dgvGranja.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvGranja.Size = new System.Drawing.Size(843, 203);
+            this.dgvGranja.TabIndex = 56;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.groupBox1.Controls.Add(this.txtCodigoPostalGranja);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lblCodigoGranja);
+            this.groupBox1.Controls.Add(this.txtCorreoGranja);
+            this.groupBox1.Controls.Add(this.btnEliminar);
+            this.groupBox1.Controls.Add(this.txtDireccionGranja);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.txtTelefonoGranja);
+            this.groupBox1.Controls.Add(this.txtNombreGranja);
+            this.groupBox1.Controls.Add(this.cboxEstadoGranja);
+            this.groupBox1.Controls.Add(this.btnCancelar);
+            this.groupBox1.Controls.Add(this.btnEditar);
+            this.groupBox1.Controls.Add(this.btnGuardar);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(8, 48);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(843, 183);
+            this.groupBox1.TabIndex = 55;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtCodigoPostalGranja
+            // 
+            this.txtCodigoPostalGranja.Location = new System.Drawing.Point(510, 61);
+            this.txtCodigoPostalGranja.Name = "txtCodigoPostalGranja";
+            this.txtCodigoPostalGranja.Size = new System.Drawing.Size(169, 20);
+            this.txtCodigoPostalGranja.TabIndex = 31;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
+            this.label3.Location = new System.Drawing.Point(404, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 16);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "Codigo Postal";
+            // 
+            // lblCodigoGranja
+            // 
+            this.lblCodigoGranja.AutoSize = true;
+            this.lblCodigoGranja.Location = new System.Drawing.Point(170, 24);
+            this.lblCodigoGranja.Name = "lblCodigoGranja";
+            this.lblCodigoGranja.Size = new System.Drawing.Size(25, 13);
+            this.lblCodigoGranja.TabIndex = 29;
+            this.lblCodigoGranja.Text = "cod";
+            // 
+            // txtCorreoGranja
+            // 
+            this.txtCorreoGranja.Location = new System.Drawing.Point(510, 19);
+            this.txtCorreoGranja.Name = "txtCorreoGranja";
+            this.txtCorreoGranja.Size = new System.Drawing.Size(169, 20);
+            this.txtCorreoGranja.TabIndex = 28;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnEliminar.Location = new System.Drawing.Point(751, 102);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(86, 35);
+            this.btnEliminar.TabIndex = 27;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // txtDireccionGranja
+            // 
+            this.txtDireccionGranja.Location = new System.Drawing.Point(173, 102);
+            this.txtDireccionGranja.Name = "txtDireccionGranja";
+            this.txtDireccionGranja.Size = new System.Drawing.Size(169, 20);
+            this.txtDireccionGranja.TabIndex = 26;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoSize = true;
+            this.textBox1.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
+            this.textBox1.Location = new System.Drawing.Point(92, 102);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(69, 16);
+            this.textBox1.TabIndex = 25;
+            this.textBox1.Text = "Dirección";
+            // 
+            // txtTelefonoGranja
+            // 
+            this.txtTelefonoGranja.Location = new System.Drawing.Point(173, 142);
+            this.txtTelefonoGranja.Name = "txtTelefonoGranja";
+            this.txtTelefonoGranja.Size = new System.Drawing.Size(169, 20);
+            this.txtTelefonoGranja.TabIndex = 20;
+            // 
+            // txtNombreGranja
+            // 
+            this.txtNombreGranja.Location = new System.Drawing.Point(173, 58);
+            this.txtNombreGranja.Name = "txtNombreGranja";
+            this.txtNombreGranja.Size = new System.Drawing.Size(169, 20);
+            this.txtNombreGranja.TabIndex = 19;
+            // 
+            // cboxEstadoGranja
+            // 
+            this.cboxEstadoGranja.FormattingEnabled = true;
+            this.cboxEstadoGranja.Items.AddRange(new object[] {
+            "Activo",
+            "Inactivo"});
+            this.cboxEstadoGranja.Location = new System.Drawing.Point(510, 102);
+            this.cboxEstadoGranja.Name = "cboxEstadoGranja";
+            this.cboxEstadoGranja.Size = new System.Drawing.Size(169, 21);
+            this.cboxEstadoGranja.TabIndex = 17;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnCancelar.Location = new System.Drawing.Point(751, 142);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(86, 35);
+            this.btnCancelar.TabIndex = 16;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnEditar.Location = new System.Drawing.Point(751, 61);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(86, 35);
+            this.btnEditar.TabIndex = 15;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnGuardar.Location = new System.Drawing.Point(751, 20);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(86, 35);
+            this.btnGuardar.TabIndex = 14;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
+            this.label7.Location = new System.Drawing.Point(450, 103);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 16);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Estado";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
+            this.label6.Location = new System.Drawing.Point(449, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Correo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
+            this.label5.Location = new System.Drawing.Point(97, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Telefono";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
+            this.label4.Location = new System.Drawing.Point(55, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nombre Granja";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(46, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Codigo Producto";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(336, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 29);
+            this.label1.TabIndex = 54;
+            this.label1.Text = "PRODUCTOS";
+            // 
+            // lblFechaGranja
+            // 
+            this.lblFechaGranja.AutoSize = true;
+            this.lblFechaGranja.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaGranja.Location = new System.Drawing.Point(763, 18);
+            this.lblFechaGranja.Name = "lblFechaGranja";
+            this.lblFechaGranja.Size = new System.Drawing.Size(55, 22);
+            this.lblFechaGranja.TabIndex = 58;
+            this.lblFechaGranja.Text = "Fecha";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(697, 18);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(60, 22);
+            this.label10.TabIndex = 57;
+            this.label10.Text = "Fecha:";
+            // 
+            // frmProducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 489);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnMenu);
+            this.Controls.Add(this.dgvGranja);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblFechaGranja);
+            this.Controls.Add(this.label10);
+            this.Name = "frmProducto";
+            this.Text = "frmProducto";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGranja)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnMenu;
+        private System.Windows.Forms.DataGridView dgvGranja;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtCodigoPostalGranja;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblCodigoGranja;
+        private System.Windows.Forms.TextBox txtCorreoGranja;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.TextBox txtDireccionGranja;
+        private System.Windows.Forms.Label textBox1;
+        private System.Windows.Forms.TextBox txtTelefonoGranja;
+        private System.Windows.Forms.TextBox txtNombreGranja;
+        private System.Windows.Forms.ComboBox cboxEstadoGranja;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblFechaGranja;
+        private System.Windows.Forms.Label label10;
+    }
+}
