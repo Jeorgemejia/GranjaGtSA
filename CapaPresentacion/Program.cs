3f81093a4a49d6215707ad33038d795e8e43f9c3
Lbgraica <garciahernandezbryan@gmail.com>
2025-10-31 00:01:52 -0600

    #Prueba

--- CapaPresentacion/Program.cs
+++ CapaPresentacion/Program.cs
@@ -16,7 +16,7 @@ namespace CapaPresentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmRoles());
+            Application.Run(new frmGranja());
         }
     }
 }
