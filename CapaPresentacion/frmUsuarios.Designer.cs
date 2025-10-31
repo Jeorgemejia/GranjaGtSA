f3394ee8360de4717144a1c0b5d38d17b31bfae5
KEscobar <kristelescobar0110@gmail.com>
2025-10-31 12:03:57 -0600

    form Uusarios RESTAURADO :))

--- CapaPresentacion/frmUsuarios.Designer.cs
+++ CapaPresentacion/frmUsuarios.Designer.cs
@@ -28,73 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.cboxtipoInsumo = new System.Windows.Forms.ComboBox();
-            this.cboxProveedor = new System.Windows.Forms.ComboBox();
-            this.txtObservacion = new System.Windows.Forms.TextBox();
-            this.label9 = new System.Windows.Forms.Label();
+            this.cboRol = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
             this.btnSalir = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.btnMenu = new System.Windows.Forms.Button();
-            this.lblCodigoInsumo = new System.Windows.Forms.Label();
-            this.txtCostoUnitarioInsumo = new System.Windows.Forms.TextBox();
+            this.lblCodigoUsuario = new System.Windows.Forms.Label();
             this.btnEliminar = new System.Windows.Forms.Button();
-            this.dgvInsumos = new System.Windows.Forms.DataGridView();
+            this.dgvUsuarios = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.lblFechaSistema = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.Label();
-            this.txtNombreInsumo = new System.Windows.Forms.TextBox();
-            this.cboxEstadoInsumo = new System.Windows.Forms.ComboBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.cboEstado = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dtpFechaRegistro = new System.Windows.Forms.DateTimePicker();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
             this.btnCancelar = new System.Windows.Forms.Button();
             this.btnEditar = new System.Windows.Forms.Button();
             this.btnGuardar = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvInsumos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // cboxtipoInsumo
+            // cboRol
             // 
-            this.cboxtipoInsumo.FormattingEnabled = true;
-            this.cboxtipoInsumo.Items.AddRange(new object[] {
-            "Consumo",
-            "Limpieza",
-            "Mantenimiento",
-            "Cocina",
-            "Librería"});
-            this.cboxtipoInsumo.Location = new System.Drawing.Point(78, 90);
-            this.cboxtipoInsumo.Name = "cboxtipoInsumo";
-            this.cboxtipoInsumo.Size = new System.Drawing.Size(169, 21);
-            this.cboxtipoInsumo.TabIndex = 37;
-            // 
-            // cboxProveedor
-            // 
-            this.cboxProveedor.FormattingEnabled = true;
-            this.cboxProveedor.Location = new System.Drawing.Point(78, 35);
-            this.cboxProveedor.Name = "cboxProveedor";
-            this.cboxProveedor.Size = new System.Drawing.Size(169, 21);
-            this.cboxProveedor.TabIndex = 36;
-            // 
-            // txtObservacion
-            // 
-            this.txtObservacion.Location = new System.Drawing.Point(645, 47);
-            this.txtObservacion.Name = "txtObservacion";
-            this.txtObservacion.Size = new System.Drawing.Size(169, 20);
-            this.txtObservacion.TabIndex = 35;
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
-            this.label9.Location = new System.Drawing.Point(688, 28);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(87, 16);
-            this.label9.TabIndex = 34;
-            this.label9.Text = "Observacion";
+            this.cboRol.FormattingEnabled = true;
+            this.cboRol.Location = new System.Drawing.Point(78, 35);
+            this.cboRol.Name = "cboRol";
+            this.cboRol.Size = new System.Drawing.Size(169, 21);
+            this.cboRol.TabIndex = 36;
             // 
             // label6
             // 
@@ -105,7 +72,6 @@
             this.label6.Size = new System.Drawing.Size(102, 16);
             this.label6.TabIndex = 9;
             this.label6.Text = "Fecha Registro";
-            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // btnSalir
             // 
@@ -117,6 +83,7 @@
             this.btnSalir.TabIndex = 60;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click_1);
             // 
             // label2
             // 
@@ -140,21 +107,14 @@
             this.btnMenu.Text = "Menu";
             this.btnMenu.UseVisualStyleBackColor = true;
             // 
-            // lblCodigoInsumo
+            // lblCodigoUsuario
             // 
-            this.lblCodigoInsumo.AutoSize = true;
-            this.lblCodigoInsumo.Location = new System.Drawing.Point(75, 20);
-            this.lblCodigoInsumo.Name = "lblCodigoInsumo";
-            this.lblCodigoInsumo.Size = new System.Drawing.Size(25, 13);
-            this.lblCodigoInsumo.TabIndex = 29;
-            this.lblCodigoInsumo.Text = "cod";
-            // 
-            // txtCostoUnitarioInsumo
-            // 
-            this.txtCostoUnitarioInsumo.Location = new System.Drawing.Point(434, 10);
-            this.txtCostoUnitarioInsumo.Name = "txtCostoUnitarioInsumo";
-            this.txtCostoUnitarioInsumo.Size = new System.Drawing.Size(169, 20);
-            this.txtCostoUnitarioInsumo.TabIndex = 28;
+            this.lblCodigoUsuario.AutoSize = true;
+            this.lblCodigoUsuario.Location = new System.Drawing.Point(75, 20);
+            this.lblCodigoUsuario.Name = "lblCodigoUsuario";
+            this.lblCodigoUsuario.Size = new System.Drawing.Size(25, 13);
+            this.lblCodigoUsuario.TabIndex = 29;
+            this.lblCodigoUsuario.Text = "cod";
             // 
             // btnEliminar
             // 
@@ -166,17 +126,19 @@
             this.btnEliminar.TabIndex = 27;
             this.btnEliminar.Text = "Eliminar";
             this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click_1);
             // 
-            // dgvInsumos
+            // dgvUsuarios
             // 
-            this.dgvInsumos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
-            this.dgvInsumos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvInsumos.Location = new System.Drawing.Point(-2, 213);
-            this.dgvInsumos.Name = "dgvInsumos";
-            this.dgvInsumos.ReadOnly = true;
-            this.dgvInsumos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvInsumos.Size = new System.Drawing.Size(843, 203);
-            this.dgvInsumos.TabIndex = 56;
+            this.dgvUsuarios.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dgvUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsuarios.Location = new System.Drawing.Point(-2, 213);
+            this.dgvUsuarios.Name = "dgvUsuarios";
+            this.dgvUsuarios.ReadOnly = true;
+            this.dgvUsuarios.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvUsuarios.Size = new System.Drawing.Size(843, 203);
+            this.dgvUsuarios.TabIndex = 56;
+            this.dgvUsuarios.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUsuarios_CellClick_1);
             // 
             // label1
             // 
@@ -219,37 +181,35 @@
             this.textBox1.TabIndex = 25;
             this.textBox1.Text = "Clave";
             // 
-            // txtNombreInsumo
+            // txtNombre
             // 
-            this.txtNombreInsumo.Location = new System.Drawing.Point(78, 63);
-            this.txtNombreInsumo.Name = "txtNombreInsumo";
-            this.txtNombreInsumo.Size = new System.Drawing.Size(169, 20);
-            this.txtNombreInsumo.TabIndex = 20;
+            this.txtNombre.Location = new System.Drawing.Point(78, 63);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(169, 20);
+            this.txtNombre.TabIndex = 20;
             // 
-            // cboxEstadoInsumo
+            // cboEstado
             // 
-            this.cboxEstadoInsumo.FormattingEnabled = true;
-            this.cboxEstadoInsumo.Items.AddRange(new object[] {
+            this.cboEstado.FormattingEnabled = true;
+            this.cboEstado.Items.AddRange(new object[] {
             "Activo",
             "Inactivo"});
-            this.cboxEstadoInsumo.Location = new System.Drawing.Point(434, 35);
-            this.cboxEstadoInsumo.Name = "cboxEstadoInsumo";
-            this.cboxEstadoInsumo.Size = new System.Drawing.Size(169, 21);
-            this.cboxEstadoInsumo.TabIndex = 17;
+            this.cboEstado.Location = new System.Drawing.Point(434, 35);
+            this.cboEstado.Name = "cboEstado";
+            this.cboEstado.Size = new System.Drawing.Size(169, 21);
+            this.cboEstado.TabIndex = 17;
             // 
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.groupBox1.Controls.Add(this.cboxtipoInsumo);
-            this.groupBox1.Controls.Add(this.cboxProveedor);
-            this.groupBox1.Controls.Add(this.txtObservacion);
-            this.groupBox1.Controls.Add(this.label9);
-            this.groupBox1.Controls.Add(this.lblCodigoInsumo);
-            this.groupBox1.Controls.Add(this.txtCostoUnitarioInsumo);
+            this.groupBox1.Controls.Add(this.dtpFechaRegistro);
+            this.groupBox1.Controls.Add(this.txtContraseña);
+            this.groupBox1.Controls.Add(this.cboRol);
+            this.groupBox1.Controls.Add(this.lblCodigoUsuario);
             this.groupBox1.Controls.Add(this.btnEliminar);
             this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.txtNombreInsumo);
-            this.groupBox1.Controls.Add(this.cboxEstadoInsumo);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.cboEstado);
             this.groupBox1.Controls.Add(this.btnCancelar);
             this.groupBox1.Controls.Add(this.btnEditar);
             this.groupBox1.Controls.Add(this.btnGuardar);
@@ -264,6 +224,20 @@
             this.groupBox1.TabIndex = 55;
             this.groupBox1.TabStop = false;
             // 
+            // dtpFechaRegistro
+            // 
+            this.dtpFechaRegistro.Location = new System.Drawing.Point(434, 9);
+            this.dtpFechaRegistro.Name = "dtpFechaRegistro";
+            this.dtpFechaRegistro.Size = new System.Drawing.Size(200, 20);
+            this.dtpFechaRegistro.TabIndex = 38;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(78, 89);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(169, 20);
+            this.txtContraseña.TabIndex = 37;
+            // 
             // btnCancelar
             // 
             this.btnCancelar.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -274,6 +248,7 @@
             this.btnCancelar.TabIndex = 16;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click_1);
             // 
             // btnEditar
             // 
@@ -285,6 +260,7 @@
             this.btnEditar.TabIndex = 15;
             this.btnEditar.Text = "Editar";
             this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click_1);
             // 
             // btnGuardar
             // 
@@ -296,6 +272,7 @@
             this.btnGuardar.TabIndex = 14;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click_1);
             // 
             // label7
             // 
@@ -334,7 +311,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.btnMenu);
-            this.Controls.Add(this.dgvInsumos);
+            this.Controls.Add(this.dgvUsuarios);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblFechaSistema);
             this.Controls.Add(this.label10);
@@ -342,7 +319,7 @@
             this.Name = "frmUsuarios";
             this.Text = "frmUsuarios";
             this.Load += new System.EventHandler(this.frmUsuarios_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvInsumos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -351,25 +328,20 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.ComboBox cboxtipoInsumo;
-        private System.Windows.Forms.ComboBox cboxProveedor;
-        private System.Windows.Forms.TextBox txtObservacion;
-        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox cboRol;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button btnSalir;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnMenu;
-        private System.Windows.Forms.Label lblCodigoInsumo;
-        private System.Windows.Forms.TextBox txtCostoUnitarioInsumo;
+        private System.Windows.Forms.Label lblCodigoUsuario;
         private System.Windows.Forms.Button btnEliminar;
-        private System.Windows.Forms.DataGridView dgvInsumos;
+        private System.Windows.Forms.DataGridView dgvUsuarios;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblFechaSistema;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label textBox1;
-        private System.Windows.Forms.TextBox txtNombreInsumo;
-        private System.Windows.Forms.ComboBox cboxEstadoInsumo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.ComboBox cboEstado;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnCancelar;
         private System.Windows.Forms.Button btnEditar;
@@ -377,5 +349,7 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.DateTimePicker dtpFechaRegistro;
     }
 }
