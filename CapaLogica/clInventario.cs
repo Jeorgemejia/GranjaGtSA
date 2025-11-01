43f7dc69d121c0d3154931fa0989b14a7c23896e
cmejia <cmejiaj@miumg.edu.gt>
2025-10-31 19:34:26 -0600

    Frminventario terminadoy clInventario

---
+++ CapaLogica/clInventario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogica
+{
+    public class clInventario
+    {
+        public decimal mtdTotalCosto( decimal CantidadDisponible, decimal CostoUnitario)
+        {
+            decimal CostoTotal;
+            CostoTotal = CantidadDisponible * CostoUnitario;
+            return CostoTotal;
+
+
+        }
+
+    }
+}
