0eeef7a6614e8e6f2d9d85a3369b952bde29efa7
KEscobar <kristelescobar0110@gmail.com>
2025-10-31 19:24:37 -0600

    MENU PRINCIPAL con el botón de Roles, listo :)

---
+++ CapaPresentacion/frmMenuPrincipal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    public partial class frmMenuPrincipal : Form
+    {
+        public frmMenuPrincipal()
+        {
+            InitializeComponent();
+        }
+        private void frmMenuPrincipal_Load(object sender, EventArgs e)
+        {
+          
+            AbrirFormulario<frmRoles>();
+        }
+
+        
+        private void AbrirFormulario<MiForm>() where MiForm : Form, new()
+        {
+          Form formulario = panelMenu.Controls.OfType<MiForm>().FirstOrDefault();
+            if (formulario == null)
+            {
+                formulario = new MiForm();
+                formulario.TopLevel = false;
+                formulario.FormBorderStyle = FormBorderStyle.None;
+                formulario.Dock = DockStyle.Fill;
+                panelMenu.Controls.Add(formulario);
+                panelMenu.Tag = formulario;
+                formulario.Show();          // mostrar por primera vez
+                formulario.BringToFront();
+            }
+            else
+            {
+                formulario.Show();          // mostrar si estaba oculto
+                formulario.BringToFront();  // traer al frente
+            }
+        }
+
+        
+        
+
+        private void btnRoles_Click_1(object sender, EventArgs e)
+        {
+            AbrirFormulario<frmRoles>();
+        }
+    }
+}
+    
