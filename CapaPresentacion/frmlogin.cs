4bb4f7d9a0a7c476f2186e2c008999c46400634e
KEscobar <kristelescobar0110@gmail.com>
2025-11-01 22:32:19 -0600

    login empezado

---
+++ CapaPresentacion/frmlogin.cs
@@ -0,0 +1,149 @@
+﻿using CapaSeguridad;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using CapaSeguridad;
+
+namespace CapaPresentacion
+{
+    public partial class frmlogin : Form
+    {
+        public frmlogin()
+        {
+            InitializeComponent();
+        }
+
+        // Para mover la ventana desde el panel
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(IntPtr hwnd, int wmsg, int wparam, int lparam);
+
+        private void Panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void frmlogin_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        // BOTÓN LOGIN
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtuser.Text) || txtuser.Text == "Username")
+            {
+                msgError("Ingrese el usuario.");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(txtpass.Text) || txtpass.Text == "Password")
+            {
+                msgError("Ingrese la contraseña.");
+                return;
+            }
+
+            UserModel user = new UserModel();
+            bool validLogin = user.LoginUser(txtuser.Text, txtpass.Text);
+
+            if (validLogin)
+            {
+                frmMenuPrincipal mainMenu = new frmMenuPrincipal();
+                mainMenu.Show();
+                mainMenu.FormClosed += Logout;
+                this.Hide();
+            }
+            else
+            {
+                msgError("Usuario o contraseña incorrecta.");
+                txtpass.Text = "Password";
+                txtpass.UseSystemPasswordChar = false;
+                txtuser.Focus();
+            }
+        }
+
+        // PLACEHOLDER DE PASSWORD
+        private void txtpass_Enter(object sender, EventArgs e)
+        {
+            if (txtpass.Text == "Password")
+            {
+                txtpass.Text = "";
+                txtpass.ForeColor = Color.LightGray;
+                txtpass.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtpass_Leave(object sender, EventArgs e)
+        {
+            if (txtpass.Text == "")
+            {
+                txtpass.Text = "Password";
+                txtpass.ForeColor = Color.Silver;
+                txtpass.UseSystemPasswordChar = false;
+            }
+        }
+
+        // PLACEHOLDER DE USUARIO
+        private void txtuser_Enter(object sender, EventArgs e)
+        {
+            if (txtuser.Text == "Username")
+            {
+                txtuser.Text = "";
+                txtuser.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void txtuser_Leave(object sender, EventArgs e)
+        {
+            if (txtuser.Text == "")
+            {
+                txtuser.Text = "Username";
+                txtuser.ForeColor = Color.Silver;
+            }
+        }
+
+        // MOSTRAR MENSAJE DE ERROR
+        private void msgError(string msg)
+        {
+            lblErrorMessage.Text = "    " + msg;
+            lblErrorMessage.Visible = true;
+        }
+
+        // LOGOUT: vuelve al login cuando se cierra el menú principal
+        private void Logout(object sender, FormClosedEventArgs e)
+        {
+            txtpass.Text = "Password";
+            txtpass.UseSystemPasswordChar = false;
+            txtuser.Text = "Username";
+            lblErrorMessage.Visible = false;
+            this.Show();
+        }
+
+        // MINIMIZAR
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        // CERRAR APLICACIÓN
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ CapaSeguridad/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaSeguridad
+{
+    public class UserModel
+    {
+        private UserDao userDao = new UserDao();
+
+        public bool LoginUser(string user, string pass)
+        {
+            return userDao.Login(user, pass);
+        }
+    }
+
+}
